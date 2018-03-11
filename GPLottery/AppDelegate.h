8181ea8991be79113b3d47e65adef4509b3c839a
CuiNing001 <380693205@qq.com>
2018-03-11 09:05:51 +0800

    GPLottery

---
+++ GPLottery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GPLottery
+//
+//  Created by cc on 2018/3/11.
+//  Copyright © 2018年 cc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
